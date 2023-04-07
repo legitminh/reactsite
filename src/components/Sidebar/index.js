c8aab69cb16ddbf2d83b353d7c0af489455262b3
legitMinh <totallylegitperson.example@gmail.com>
2023-04-07 14:19:48 -0400

    set up fonts and paths

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+const Sidebar = () => {
+    // <div className = 'nav-bar'>
+    //     <Link className = 'logo' to = '/='>
+    //         <img src = {LogoS} alt = 'logo'/>
+    //         <img src = {LogoSubtitle} alt = 'slobodan'/>
+    //     </Link>
+
+    // </div>
+}
+export default Sidebar   
