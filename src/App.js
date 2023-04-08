317c11822a5f49df9013454756ccb968d2f37790
legitMinh <totallylegitperson.example@gmail.com>
2023-04-08 15:08:21 -0400

    add side bar but can't render yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout'
+import './App.scss';
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path = '/' elemt = {<Layout />}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Route>
+      </Routes>
     </>
   );
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,10 @@
 import Sidebar from '../Sidebar'
 import './index.scss'
 const Layout = () => {
-    return <Sidebar/>
+    return (
+        <>
+            <Sidebar/>
+        </>
+    )
 }
 export default Layout 
