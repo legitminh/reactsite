d0bdc0c8a9d6d4934b516ee0eb491bcca224390e
legitMinh <totallylegitperson.example@gmail.com>
2023-04-10 06:22:15 -0400

    sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,21 +3,29 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faHome} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => 
 {
     // return <>Hello</>
-    <div className = "nav-bar">
-        <Link className = "logo" to = "/">
-            <img src = {LogoS} alt = 'logo'/>
-            <img className="sub-logo" src =  {LogoSubtitle} alt = "slobodan"/>
-        </Link>
-        <nav>
-            <NavLink exact = "true" activeclassname = "active" to= "/">
-                <FontAwesomeIcon icon = {faHome} color = "4d4d4e"/>
-            </NavLink>
-        </nav>
-    </div>
+    return(
+        <div className = "nav-bar">
+            <Link className = "logo" to = "/">
+                <img src = {LogoS} alt = 'logo'/>
+                <img className="sub-logo" src =  {LogoSubtitle} alt = "slobodan"/>
+            </Link>
+            <nav>
+                <NavLink exact = "true" activeclassname = "active" to= "/">
+                    <FontAwesomeIcon icon = {faHome} color = "#4d4d4e"/>
+                </NavLink>
+                <NavLink exact = "true" activeclassname = "active" className = "about" to= "/about">
+                    <FontAwesomeIcon icon = {faUser} color = "#4d4d4e"/>
+                </NavLink>
+                <NavLink exact = "true" activeclassname = "contact-link" to= "/contact">
+                    <FontAwesomeIcon icon = {faEnvelope} color = "#4d4d4e"/>
+                </NavLink>
+            </nav>
+        </div>
+    )
 }
 export default Sidebar
